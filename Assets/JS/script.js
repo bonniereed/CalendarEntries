c1fe16dece0a2ba2378158e40b6d337b8191449f
Bonnie Reed <bonniejeanreed@gmail.com>
2021-09-09 14:11:50 -0500

    update input tags and buttons

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,9 +1,34 @@
 var rootEl = $("#root");
 var dayDisplayEl = $("#currentDay");
 var containerEl = $("#container");
-var formData = new FormData();
+//Form elements
+var formElOne = $("#one");
+var formElTwo = $("#two");
+var formElThree = $("#three");
+var formElFour = $("#four");
+var formElFive = $("#five");
+var formElSix = $("#six");
+var formElSeven = $("#seven");
+var formElEight = $("#eight");
+var formElNine = $("#nine");
+var formElTen = $("#ten");
+var formElEleven = $("#eleven");
+var formElTwelve = $("#twelve");
 var calendarInput = $(".calendarInput");
 var timeEntry = $(".timeEntry");
+//calendar list item elements
+var calListEight = $("#calApptEight");
+var calListNine = $("#calApptNine");
+var calListTen = $("#calApptTen");
+var calListEleven = $("#calApptEleven");
+var calListTwelve = $("#calApptTwelve");
+var calListOne = $("#calApptOne");
+var calListTwo = $("#calApptTwo");
+var calListThree = $("#calApptThree");
+var calListFour = $("#calApptFour");
+var calListFive = $("#calApptFive");
+var calListSix = $("#calApptSix");
+var calListSeven = $("#calApptSeven");
 
 function displayTime() {
   Date.prototype.getDay();
@@ -17,23 +42,54 @@ function handleFormSubmit(event) {
   event.preventDefault();
 
   // select form element by its `name` attribute and get its value
-  calendarInput = $('input[name="calendar-input"]').val();
+  var calendarAppt = $('input[name="calendar-input"]').val();
 
   // if there's nothing in the form entered, don't print to the page
-  if (!calendarInput) {
+  if (!calendarAppt) {
     console.log("No shopping item filled out in form!");
     return;
   }
 
   // print to the page
-  timeEntry.append("<li>" + calendarInput + "</li>");
-
-  // clear the form input element
+  calListEight.append("<li>" + calendarAppt + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListNine.append("<li>" + calendarInputNine + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListTen.append("<li>" + calendarInputTen + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListEleven.append("<li>" + calendarInputEleven + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListTwelve.append("<li>" + calendarInputTwelve + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListOne.append("<li>" + calendarInputOne + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListTwo.append("<li>" + calendarInputTwo + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListThree.append("<li>" + calendarInputThree + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListFour.append("<li>" + calendarInputFour + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListFive.append("<li>" + calendarInputFive + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListSix.append("<li>" + calendarInputSix + "</li>");
+  $('input[name="calendar-input"]').val("");
+  calListSeven.append("<li>" + calendarInputSeven + "</li>");
   $('input[name="calendar-input"]').val("");
 }
 
 // Create a submit event listener on the form element
-timeEntry.on("submit", handleFormSubmit);
+formElEight.on("submit", handleFormSubmit);
+formElNine.on("submit", handleFormSubmit);
+formElTen.on("submit", handleFormSubmit);
+formElEleven.on("submit", handleFormSubmit);
+formElTwelve.on("submit", handleFormSubmit);
+formElOne.on("submit", handleFormSubmit);
+formElTwo.on("submit", handleFormSubmit);
+formElThree.on("submit", handleFormSubmit);
+formElFour.on("submit", handleFormSubmit);
+formElFive.on("submit", handleFormSubmit);
+formElSix.on("submit", handleFormSubmit);
+formElSeven.on("submit", handleFormSubmit);
 
 // var twelveTimeEntry = containerEl.append(formEl);
 // var oneTimeEntry = twelveTimeEntry.append(formEl);
